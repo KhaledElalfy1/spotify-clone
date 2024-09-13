f1dea26f0335d76f282ecd497c33c0ad6cf01ca6
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-09-13 15:51:12 +0300

    add fetch songs from db

--- lib/features/home/repositories/home_repository.dart
+++ lib/features/home/repositories/home_repository.dart
@@ -1,30 +1,28 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
 import 'package:client/core/failure/failure.dart';
 import 'package:client/core/helpers/server_constant.dart';
+import 'package:client/features/home/model/song_model.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'home_repository.g.dart';
-@riverpod
 
-HomeRepository homeRepository(HomeRepositoryRef ref){
+@riverpod
+HomeRepository homeRepository(HomeRepositoryRef ref) {
   return HomeRepository();
 }
 
-
-
-
-
 class HomeRepository {
   Future<Either<AppFailure, String>> uploadSong(
-      {
-    required File selectedAudio,
-   required File selectedThumbnail,
-  required String songName,
-  required String artist,
-  required String hexCode,required String token}) async {
+      {required File selectedAudio,
+      required File selectedThumbnail,
+      required String songName,
+      required String artist,
+      required String hexCode,
+      required String token}) async {
     try {
       final request = http.MultipartRequest(
           'POST', Uri.parse('${ServerConstant.baseUrl}/song/upload'));
@@ -33,7 +31,8 @@ class HomeRepository {
         ..files.addAll(
           [
             await http.MultipartFile.fromPath('song', selectedAudio.path),
-            await http.MultipartFile.fromPath('thumbnail', selectedThumbnail.path),
+            await http.MultipartFile.fromPath(
+                'thumbnail', selectedThumbnail.path),
           ],
         )
         ..fields.addAll(
@@ -45,21 +44,45 @@ class HomeRepository {
         )
         ..headers.addAll(
           {
-            'x-auth-token':
-                token,
+            'x-auth-token': token,
           },
         );
 
       final response = await request.send();
 
       log(response.statusCode.toString());
-      if(response.statusCode != 201) {
-        return left(AppFailure(message:await response.stream.bytesToString()));
+      if (response.statusCode != 201) {
+        return left(AppFailure(message: await response.stream.bytesToString()));
       }
       log(await response.stream.bytesToString());
       return right(await response.stream.bytesToString());
     } catch (e) {
       return left(AppFailure(message: e.toString()));
+    }
+  }
+
+  Future<Either<AppFailure, List<SongModel>>> getAllSongs(
+      {required String token}) async {
+    try {
+      final response = await http
+          .get(Uri.parse('${ServerConstant.baseUrl}/song/list'), headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': token,
+      });
+      var responseBody = jsonDecode(response.body);
+      log(response.statusCode.toString());
+      if (response.statusCode != 200) {
+        responseBody = responseBody as Map<String, dynamic>;
+        return Left(AppFailure(message: responseBody['detail']));
+      }
+      responseBody = responseBody as List<dynamic>;
+      List<SongModel> songs = [];
+      for (var map in responseBody) {
+        songs.add(SongModel.fromMap(map));
+      }
+      return Right(songs);
+    } catch (e) {
+      return Left(AppFailure(message: e.toString()));
     }
   }
 }
