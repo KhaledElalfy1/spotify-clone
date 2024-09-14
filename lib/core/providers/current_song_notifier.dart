51c3c27acd3a2c0fe9dc553bf41cd507fba279b0
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-09-14 07:52:13 +0300

    add song player functionality

--- lib/core/providers/current_song_notifier.dart
+++ lib/core/providers/current_song_notifier.dart
@@ -6,6 +6,7 @@ part 'current_song_notifier.g.dart';
 @riverpod
 class CurrentSongNotifier extends _$CurrentSongNotifier {
   AudioPlayer? audioPlayer;
+  bool isPlaying = false;
   @override
   SongModel? build() {
     return null;
@@ -13,9 +14,29 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
 
   void updateSong(SongModel song) async {
     audioPlayer = AudioPlayer();
-    final audioSource=AudioSource.uri(Uri.parse(song.song_url));
+    final audioSource = AudioSource.uri(Uri.parse(song.song_url));
     await audioPlayer!.setAudioSource(audioSource);
-    await audioPlayer!.play();
-    state=song;
+    audioPlayer!.playerStateStream.listen((event) {
+      if (event.processingState == ProcessingState.completed) {
+        audioPlayer!.seek(Duration.zero);
+        audioPlayer!.pause();
+        isPlaying = false;
+        state = state?.copyWith(hex_code: state?.hex_code);
+      }
+    });
+    audioPlayer!
+        .play(); // Don't use await here, as if you use await,song will appear after it finishes playing
+    isPlaying = true;
+    state = song;
+  }
+
+  void playPause() {
+    if (isPlaying) {
+      audioPlayer?.pause();
+    } else {
+      audioPlayer?.play();
+    }
+    isPlaying = !isPlaying;
+    state = state?.copyWith(hex_code: state?.hex_code);
   }
 }
---
+++ lib/features/home/view/pages/widgets/music_slab.dart
@@ -0,0 +1,129 @@
+import 'package:client/core/helpers/functions.dart';
+import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MusicSlab extends ConsumerWidget {
+  const MusicSlab({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentSongs = ref.watch(currentSongNotifierProvider);
+    final songNotifier = ref.read(currentSongNotifierProvider.notifier);
+    if (currentSongs == null) {
+      return const SizedBox();
+    }
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(10),
+          height: 66,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: BoxDecoration(
+            color: hexToColor(currentSongs.hex_code),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 46,
+                    width: 46,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      image: DecorationImage(
+                        image: NetworkImage(currentSongs.thumbnail_url),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        currentSongs.song_name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        currentSongs.artist,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.subtitleText,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.favorite_border,
+                      color: AppColors.whiteColor,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: songNotifier.playPause,
+                    icon: Icon(
+                      songNotifier.isPlaying ? Icons.pause : Icons.play_arrow,
+                      color: AppColors.whiteColor,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 8,
+          child: Container(
+            height: 2,
+            width: MediaQuery.sizeOf(context).width - 16,
+            decoration: BoxDecoration(
+              color: AppColors.inactiveBottomBarItemColor,
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+        ),
+        StreamBuilder(
+          stream: songNotifier.audioPlayer?.positionStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SizedBox();
+            }
+            final position = snapshot.data;
+            final duration = songNotifier.audioPlayer!.duration;
+            double sliderValue = 0.0;
+            if (position != null && duration != null) {
+              sliderValue = position.inMilliseconds / duration.inMilliseconds;
+            }
+            return Positioned(
+              bottom: 0,
+              left: 8,
+              child: Container(
+                height: 2,
+                width: sliderValue * (MediaQuery.sizeOf(context).width - 16),
+                decoration: BoxDecoration(
+                  color: AppColors.whiteColor,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
