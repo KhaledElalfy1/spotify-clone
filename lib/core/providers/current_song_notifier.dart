7ad6893756c59e626564ad332f4afe4c30036587
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-09-21 12:10:26 +0300

    add background music playing

--- lib/core/providers/current_song_notifier.dart
+++ lib/core/providers/current_song_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:client/features/home/model/song_model.dart';
 import 'package:client/features/home/repositories/home_local_repository.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:just_audio/just_audio.dart';
 part 'current_song_notifier.g.dart';
@@ -18,16 +19,26 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
   void updateSong(SongModel song) async {
     await audioPlayer?.stop();
     audioPlayer = AudioPlayer();
-    final audioSource = AudioSource.uri(Uri.parse(song.song_url));
+    final audioSource = AudioSource.uri(
+      Uri.parse(song.song_url),
+      tag: MediaItem(
+        id: song.id,
+        title: song.song_name,
+        artist: song.artist,
+        artUri: Uri.parse(song.thumbnail_url),
+      ),
+    );
     await audioPlayer!.setAudioSource(audioSource);
-    audioPlayer!.playerStateStream.listen((event) {
-      if (event.processingState == ProcessingState.completed) {
-        audioPlayer!.seek(Duration.zero);
-        audioPlayer!.pause();
-        isPlaying = false;
-        state = state?.copyWith(hex_code: state?.hex_code);
-      }
-    });
+    audioPlayer!.playerStateStream.listen(
+      (event) {
+        if (event.processingState == ProcessingState.completed) {
+          audioPlayer!.seek(Duration.zero);
+          audioPlayer!.pause();
+          isPlaying = false;
+          state = state?.copyWith(hex_code: state?.hex_code);
+        }
+      },
+    );
     _homeLocalRepository.uploadLocalSong(song);
     audioPlayer!
         .play(); // Don't use await here, as if you use await,song will appear after it finishes playing
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:client/features/auth/presentation/controller/auth_viewmodel.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
-
+import 'package:just_audio_background/just_audio_background.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
   final container = ProviderContainer();
   await Hive.initFlutter();
   await Hive.openBox('songs');
